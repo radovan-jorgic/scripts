0b564b417534d484f807b044be458052feb785a1
Radovan <radovanjorgic14@gmail.com>
2023-12-01 16:00:00 +0100

    zoho script

--- functions/zoho.ts
+++ functions/zoho.ts
@@ -1,13 +1,14 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosResponse, AxiosError } from "axios";
 import * as readline from "readline";
 
 const API_URL = "https://projectsapi.zoho.in/restapi";
 const ACCOUNTS_URL = "https://accounts.zoho.in";
-const CLIENT_ID = "1000.DIDR2WOZ3HZRLQJFS811YV6ZF2AFJY";
-const CLIENT_SECRET = "0c4a618c70e420e4a019d1a4387cdfb746e12f16a0";
 const REDIRECT_URI = "https://app.dev.devrev-eng.ai/login";
 
-let accessToken = ""; // --> enter access token if you already have it
+const clientId = "1000.DIDR2WOZ3HZRLQJFS811YV6ZF2AFJY"; // enter client secret frpm zoho api console
+const clientSecret = "0c4a618c70e420e4a019d1a4387cdfb746e12f16a0"; // enter correct client secret from zoho api console
+let accessToken =
+  "1000.3a0d7e82257ad5c43a0f25621a639442.97e79b6e5c310f0e1eab38b2febd3c10"; // enter valid access token, if you don't have it this script will generate it for you on npm start
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -15,44 +16,46 @@ const rl = readline.createInterface({
 });
 
 export default async function zoho() {
-  if (!accessToken) {
-    try {
-      const response = await createAccessTokenUsingCode();
-      const { access_token } = response.data;
+  if (accessToken) {
+    const response = await getPortals(accessToken);
 
-      console.log(`
-          Access token: ${access_token}
-        `);
-    } catch (err) {
-      console.error("Error while user authorization request", err);
-    }
+    console.log(response);
+  } else {
+    const response = await getAccessToken();
+    console.log(response);
   }
 }
 
-async function getPortals(accessToken: string): Promise<AxiosResponse> {
-  return axios.get(`${API_URL}/portals`, {
-    headers: { Authorization: `Bearer ${accessToken}` },
-  });
+async function getPortals(
+  accessToken: string
+): Promise<AxiosResponse<any, any> | AxiosError<any>> {
+  try {
+    const response = axios.get(`${API_URL}/portals`, {
+      headers: { Authorization: `Bearer ${accessToken}` },
+    });
+
+    return response;
+  } catch (err: any) {
+    return err;
+  }
 }
 
-async function getAccessToken(refreshToken: string): Promise<AxiosResponse> {
-  const URL = `${ACCOUNTS_URL}/oauth/v2/token?refresh_token=${refreshToken}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=refresh_token`;
-
-  return axios.post(URL);
-}
-
-async function createAccessTokenUsingCode(): Promise<AxiosResponse> {
-  const AUTH_URL = `${ACCOUNTS_URL}/oauth/v2/auth?scope=ZohoProjects.projects.ALL,ZohoProjects.tasks.ALL,ZohoProjects.portals.ALL&client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}`;
+async function getAccessToken(): Promise<
+  AxiosResponse<any, any> | AxiosError<any>
+> {
+  const AUTH_URL = `${ACCOUNTS_URL}/oauth/v2/auth?scope=ZohoProjects.projects.ALL,ZohoProjects.tasks.ALL,ZohoProjects.portals.ALL&client_id=${clientId}&response_type=code&redirect_uri=${REDIRECT_URI}`;
 
   console.log(`1. Open url: ${AUTH_URL}`);
-
-  const CODE = await new Promise((resolve) =>
-    rl.question("2. Enter code from url when Accept is clicked: ", resolve)
+  const CODE = await new Promise<string>((resolve) =>
+    rl.question("2. Enter code: ", resolve)
   );
-
   rl.close();
 
-  const TOKEN_URL = `${ACCOUNTS_URL}/oauth/v2/token?code=${CODE}&redirect_uri=${REDIRECT_URI}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=authorization_code`;
-
-  return axios.post(TOKEN_URL);
+  const TOKEN_URL = `${ACCOUNTS_URL}/oauth/v2/token?code=${CODE}&redirect_uri=${REDIRECT_URI}&client_id=${clientId}&client_secret=${clientSecret}&grant_type=authorization_code`;
+  try {
+    const response = axios.post(TOKEN_URL);
+    return response;
+  } catch (err: any) {
+    return err;
+  }
 }
